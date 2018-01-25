6fc67f5fafebb5645d7a87d494ed825ebcdc8760
DhruvManchala <dhruv@orchester.io>
2018-01-25 12:36:46 +0530

    using accounts-ui, removed existing sign up code.

--- imports/ui/pages/sign-up.js
+++ imports/ui/pages/sign-up.js
@@ -3,23 +3,3 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import { Accounts } from 'meteor/accounts-base';
 
 import './sign-up.html';
-
-Template.app_signUp.onCreated(function signUpOnCreated() {
-});
-
-Template.app_signUp.helpers({
-});
-
-Template.app_signUp.events({
-  'submit form'(event, instance) {
-
-    event.preventDefault();
-
-    Accounts.createUser({
-      email: event.target.email.value,
-      password: event.target.password.value
-    }, (err, res) => {
-      console.log(err);
-    })
-  },
-});
