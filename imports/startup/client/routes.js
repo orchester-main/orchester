5a32a6c41a49844b9d974a618a635941363e6c3a
DhruvManchala <dhruv@orchester.io>
2018-01-25 16:57:26 +0530

    added a template for the partner application page, that only loads some content if the user is signed in.

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -2,11 +2,19 @@ import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 
 import '../../ui/layouts/app-body.html';
+import '../../ui/pages/partner-apply.html'
 import '../../ui/pages/sign-up.js';
 
 FlowRouter.route('/sign-up', {
   name: 'App.sign-up',
   action() {
     BlazeLayout.render('app_body', { main: 'app_signUp' });
+  },
+});
+
+FlowRouter.route('/partner-apply', {
+  name: 'App.partner-apply',
+  action() {
+    BlazeLayout.render('app_body', { main: 'app_partnerApply' });
   },
 });
